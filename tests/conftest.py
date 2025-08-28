883695eec1622183f2bf01b0b3a1ee99f0dd2cfd
Marius Lange <marius.lange@bsse.ethz.ch>
2025-08-28 13:04:34 +0200

    Start to look into tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,38 @@
-import anndata as ad
-import numpy as np
 import pytest
+import scanpy as sc
 
 
 @pytest.fixture
-def adata():
-    adata = ad.AnnData(X=np.array([[1.2, 2.3], [3.4, 4.5], [5.6, 6.7]]).astype(np.float32))
-    adata.layers["scaled"] = np.array([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]]).astype(np.float32)
+def lung_data():
+    """Lung atlas dataset fixture.
+
+    Dissociated scRNA-seq dataset, used for demonstration in scIB metrics package.
+    32,472 cells × 15,148 genes across multiple batches (`.obs['batch']`), includes cell-type labels (`.obs['cell_type']`).
+    Raw counts are in `layers['counts']`.
+    """
+    adata = sc.read("data/lung_atlas.h5ad", backup_url="https://figshare.com/ndownloader/files/24539942")
+
+    # Simple preprocessing
+    sc.pp.highly_variable_genes(adata, n_top_genes=2000, flavor="cell_ranger", batch_key="batch")
+    sc.tl.pca(adata, n_comps=30, mask_var="highly_variable")
+
+    return adata
+
+
+@pytest.fixture
+def spatial_data():
+    """Spatial transcriptomics dataset fixture.
+
+    Spatial data (seqFISH) from Lohoff et al (Nature Biotech 2022), profiling mouse embryogenesis at E8.5.
+    51,787 cells × 351 genes across three slices (`.obs['embryo']`), includes cell-type labels (`.obs['celltype_harmonized']`).
+    PCA has been pre-computed and is in `.obsm['X_pca']`, spatial coordinates are in `.obsm['spatial']`.
+    """
+    adata = sc.read("data/spatial_data.h5ad", backup_url="https://figshare.com/ndownloader/files/54145250")
+
+    # Simple preprocessing
+    adata.layers["counts"] = adata.X.copy()
+
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
 
     return adata
