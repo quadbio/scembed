984be26b963fa15ea01d2e3959027fb4d3151f98
Marius Lange <marius.lange@bsse.ethz.ch>
2025-08-28 11:52:53 +0200

    Initialize the package

--- src/scembed/__init__.py
+++ src/scembed/__init__.py
@@ -1,7 +1,11 @@
+"""Single-cell embedding comparison utilities for slurm_sweep examples."""
+
 from importlib.metadata import version
 
-from . import pl, pp, tl
+from .aggregation import scIBAggregator
+from .evaluation import IntegrationEvaluator
+from .factory import get_method_instance
 
-__all__ = ["pl", "pp", "tl"]
+__all__ = ["IntegrationEvaluator", "scIBAggregator", "get_method_instance"]
 
 __version__ = version("scembed")
